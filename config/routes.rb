8da560d244bed7032f9c0f67540d61475ed2cc2f
Sophie Lejeune <sophie.shadi.lejeune@gmail.com>
2019-11-13 15:10:51 +1100

    create reviews and restaurants webpages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:new, :create]
+  end
 end
+
+
+# A visitor can see the list of all restaurants.
+# GET "restaurants"
+# She/He can add a new restaurant, and be redirected to the show view of that new restaurant.
+# GET "restaurants/new"
+# POST "restaurants"
+# She/He can see the details of a restaurant, with all the reviews related to the restaurant.
+# GET "restaurants/38"
+# She/He can add a new review to a restaurant
+# GET "restaurants/38/reviews/new"
+# POST "restaurants/38/reviews"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-uts 'Cleaning database...'
+puts 'Cleaning database of restaurants...'
 Restaurant.destroy_all
 
 puts 'Creating restaurants...'
@@ -13,24 +13,65 @@ restaurants_attributes = [
   {
     name:         'Dishoom',
     address:      '7 Boundary St, London E2 7JE',
-    phono:  'Buzzy destination for Indian street food in Bombay-style vintage decor.',
-    stars:        5,
-    chef:         'Dordon Gamsey'
+    phone_number: '03 84 57 09 65',
+    category:     'chinese'
   },
   {
     name:         'Pizza East',
     address:      '56A Shoreditch High St, London E1 6PQ',
-    description:  'Pizzeria with industrial looks, serving rustic pizza and antipasti.',
-    stars:        4,
-    chef:         'Vincent LaFromage'
+    phone_number: '03 45 67 09 45',
+    category:     'belgian'
   },
   {
-    name:         'Miznon',
-    address:      'Melbs',
-    description:  'Delicious, Israeli streed food, yum yum',
-    stars:         5,
-    chef:         'Gin Atkins'
-  }
+    name:         'Chez Sophie',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  '03 45 67 45 67',
+    category:      'japanese'
+  },
+    {
+    name:         'LeRestoBonjour',
+    address:      '5 rue des pages',
+    phone_number:  '03 45 67 09 45',
+    category:      'italian'
+  },
+    {
+    name:         'MiamMiam',
+    address:      '12 quai de la Rapee',
+    phone_number:  '03 34 56 97 32',
+    category:      'french'
+  },
 ]
+
 Restaurant.create!(restaurants_attributes)
-puts 'Finished!'
+puts 'Finished! for restaurants'
+
+# ------------
+puts 'Cleaning database of reviews...'
+Review.destroy_all
+
+puts 'Creating reviews...'
+reviews_attributes = [
+  {
+    content:         'What a good restaurant',
+    rating:           5,
+    restaurant_id:    1
+  },
+    {
+    content:         'What a great restaurant',
+    rating:           4,
+    restaurant_id:    1
+  },
+    {
+    content:         'What a good restaurant',
+    rating:           3,
+    restaurant_id:    2
+  },
+      {
+    content:         'What an amazing restaurant',
+    rating:           4,
+    restaurant_id:    2
+  },
+]
+
+Review.create!(reviews_attributes)
+puts 'Finished! for reviews'
